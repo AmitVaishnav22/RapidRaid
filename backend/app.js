2ab57e764709d69f4d419dcb6b226c8fc21436bc
AmitVaishnav22 <vaishnanamit@gmail.com>
2025-01-11 16:42:23 +0530

    implement user authentication with registration and login endpoints; add error handling and response utilities

--- backend/app.js
+++ backend/app.js
@@ -1,16 +1,22 @@
 import express from 'express';
 import cors from 'cors';
-
-
+import cookieParser from "cookie-parser"
 
 const app = express();
 
 
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials:true
+}))
+app.use(express.json({limit:'16kb'}))
+app.use(express.urlencoded({extended:true,limit:'16kb'}))
+app.use(express.static("public"))
+app.use(cookieParser())    
 
-app.use(cors());
-app.get('/', (req, res) => {
-    res.send('Hello, World!');
-})
+import userRouter from "../backend/routes/user.routes.js"
+
+app.use("/api/v1/user",userRouter)
 
 export {
     app
---
+++ backend/controllers/user.controller.js
@@ -0,0 +1,63 @@
+import { User } from "../models/user.model.js";
+import { BlackListToken } from "../models/blackListToken.model.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+import { apiError } from "../utils/apiError.js";
+import { apiResponse } from "../utils/apiResponse.js";
+
+
+const registerUser= asyncHandler(async(req,res)=>{
+    // console.log(req.body)
+    const {fullname, email, password}=req.body;
+
+    if (!fullname|| !email || !password){
+        throw new apiError(400,"Invalid user credentials")
+    }
+
+    const existingUser=await User.findOne({email});
+    if (existingUser){
+        throw new apiError(400,"Email already registered")
+    }
+
+    const user=await User.create({
+        fullname,
+        email,
+        password
+    })
+
+    if(!user){
+        throw new apiError(500,"Failed to register user")
+    }
+    const token =user.generateJwt()
+    // console.log(token)
+    // console.log(user)
+    return res.status(200).json(
+        new apiResponse(200,{user:user,token},"user successfully registered")
+    )
+})
+
+
+const loginUser= asyncHandler(async(req,res)=>{
+    const {email, password}=req.body;
+    if (!email ||!password){
+        throw new apiError(400,"Invalid user credentials")
+    }
+    const user=await User.findOne({email}).select("+password")
+    if(!user){
+        throw new apiError(401,"User does not exist")
+    }
+    const isMatch=await user.isPasswordCorrect(password)
+    if(!isMatch){
+        throw new apiError(401,"Invalid password")
+    }
+    const token=user.generateJwt()
+    res.cookie('token', token)
+    return res.status(200).json(
+        new apiResponse(200,{user:user,token},"User successfully logged in")
+    )
+})
+
+
+export {
+    registerUser,
+    loginUser
+}
