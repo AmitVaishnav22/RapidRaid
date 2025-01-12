aa103401672801b4efd84467b4964a8c5c5d6e31
AmitVaishnav22 <vaishnanamit@gmail.com>
2025-01-12 17:11:27 +0530

    add CaptainSignUp component to the application

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,8 +4,7 @@ import Home from './pages/Home'
 import UserLogin from './pages/UserLogin'
 import UserSignUp from './pages/UserSignUp'
 import CaptainLogin from './pages/CaptainLogin'
-
-
+import CaptainSignUp from './pages/CaptainSignUp'
 
 function App() {
 
